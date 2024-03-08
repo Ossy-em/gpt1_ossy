033a90a3918779ab90293045ec80dbad2e491783
Ossy-em <emosinachi@gmail.com>
2024-03-08 17:27:25 +0100

    all

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './article.css';
+const Blog = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Blog
---
+++ src/containers/footers/Footers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css';
+const Footers = () => {
+  return (
+    <div>
+      Footers
+    </div>
+  )
+}
+
+export default Footers
