e0fccfe6a042b1ecf86d3be55457f996111cc33d
Ossy-em <emosinachi@gmail.com>
2024-03-09 22:59:44 +0100

    push

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './brand.css';
+
+const Brand = () => {
+
+    return(
+        <div>
+        <p>Read Full Brand</p>
+      </div>
+      )
+}
+  
+export default Brand;
