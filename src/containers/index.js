ba544868a3446b53d36c81b0c2fb61b5420d2f3d
Ossy-em <emosinachi@gmail.com>
2024-03-14 23:27:20 +0100

    updates

--- src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Blog } from '../containers/blog/Blog'; // Adjust the path
-export { default as Features } from '../containers/features/Features'; // Adjust the path
-export { default as Footer } from '../containers/footer/Footer'; // Adjust the path
-export { default as Header } from '../containers/header/Header'; // Adjust the path and export statement
-export { default as Possibility } from '../containers/possibility/Possibility'; // Adjust the path
-export { default as WhatGPT3 } from '../containers/whatGPT3/WhatGPT3'; // Adjust the path
