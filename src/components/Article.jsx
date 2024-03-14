80642c3343ff697bf5872e8c80584750c7dd5dd6
Ossy-em <emosinachi@gmail.com>
2024-03-14 23:55:55 +0100

    commit

---
+++ src/components/Article.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './article.css';
+
+const Article =()=> {
+    return(
+        <div>
+        <p>Read Full Article</p>
+      </div>
+      )
+}
+      
+  
+export default Article;
