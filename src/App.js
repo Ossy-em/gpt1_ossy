d2bb8ec2c783cd303f87f882b841490151f987cf
Ossy-em <emosinachi@gmail.com>
2024-03-15 02:39:35 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import Brand  from './components/brand/Brand'
 import Feature from './components/feature/Feature'
 import CTA from './components/cta/CTA'
 import Navbar from './components/navbar/Navbar'
+
+import Blog from './containers/blog/Blog'
+import  Features from './containers/features/Features'
+// import Footer from './containers/footer/Footer'
 const App = () => {
   return (
+    <div>
     <div>
       <h1>fghjklfghjfghjk uuu</h1>
       <Brand />
       <Feature/>
       <CTA />
       <Navbar />
+   </div>
+   <Blog/>
+<Features/>
+{/* <Footer/> */}
     </div>
   )
 }
---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './blog.css';
+
+const Blog=()=> {
+    return(
+        <div>
+        <p>Read Full Blog.</p>
+      </div>
+      )
+}
+      
+  
+export default Blog;
