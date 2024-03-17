994b33b668d600c821eb1fea19ba85e8756c8c2b
Ossy-em <emosinachi@gmail.com>
2024-03-17 23:50:12 +0100

    push

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../logo.svg';
+// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../assets/logo.svg';
 import './navbar.css';
 
 const Navbar =()=> {
@@ -8,7 +8,7 @@ const Navbar =()=> {
       <div className="gpt3__navbar">
          <div className="gpt3__navbar-links">
          <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
+          <img src={logo} alt='logo'/>
       </div>
       </div>
       </div>
