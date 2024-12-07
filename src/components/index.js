32fecee2a91ee744754ad25268bc1e2b4141cd31
Ossy-em <emosinachi@gmail.com>
2024-12-07 10:12:27 +0100

    updated  index

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-// export { default as Article } from './article/Article';
-// export { default as Brand } from './brand/Brand';
-// export { default as CTA } from './cta/CTA';
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => (
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+        <p> 21 up, lagos<br /> All Rights Reserved</p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
@@ -32,14 +32,14 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
+        <p>21 up, lagos</p>
+        <p>+234 567 7895 348</p>
         <p>info@payme.net</p>
       </div>
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>@2024 GPT-3. All rights reserved.</p>
     </div>
   </div>
 );
